aa22bded53d64401fbf97daf1a92e20c7f3473f7
shanucs27 <146510096+shanucs27@users.noreply.github.com>
2026-08-22 09:35:16 +0530

    Time: 12 ms (31.76%), Space: 48.2 MB (5.68%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -1,40 +1,15 @@
+import java.util.*;
+
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        int n = nums.length;
-        int left = -1, right = -1;
+        int[] a = nums.clone();
+        Arrays.sort(a);
 
-        for (int i = 1; i < n; i++) {
-            if (nums[i] < nums[i - 1]) {
-                left = i - 1;
-                break;
-            }
-        }
+        int l = 0, r = nums.length - 1;
 
-        if (left == -1) return 0;
+        while (l < nums.length && nums[l] == a[l]) l++;
+        while (r >= 0 && nums[r] == a[r]) r--;
 
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] > nums[i + 1]) {
-                right = i + 1;
-                break;
-            }
-        }
-
-        int min = nums[left];
-        int max = nums[left];
-
-        for (int i = left; i <= right; i++) {
-            min = Math.min(min, nums[i]);
-            max = Math.max(max, nums[i]);
-        }
-
-        while (left > 0 && nums[left - 1] > min) {
-            left--;
-        }
-
-        while (right < n - 1 && nums[right + 1] < max) {
-            right++;
-        }
-
-        return right - left + 1;
+        return l > r ? 0 : r - l + 1;
     }
 }
